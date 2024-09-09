41fe49637da6e1d1791b09c7505e2271079f5dda
Juraj Grexa <juraj.grexa@noibit.com>
2024-09-09 14:14:07 +0200

    Parse version.py to avoid import dependcies for setup.py.

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,31 @@
 from setuptools import find_packages, setup
+import codecs
+import os.path
 
-from msdk.lib.settings import MSDK_VERSION
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("MSDK_VERSION"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="msdk",
     packages=find_packages(),
-    version=MSDK_VERSION,
+    version=get_version("msdk/lib/settings.py"),
     description="SDK for Malcore API",
     author="Thomas Perkins",
     author_email="contact@malcore.io",
     install_requires=["requests==2.31.0"],
     long_description=open("README.md").read(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/Internet-2-0/Malcore-SDK"
+    long_description_content_type="text/markdown",
+    url="https://github.com/Internet-2-0/Malcore-SDK",
 )
